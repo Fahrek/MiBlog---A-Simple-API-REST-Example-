503d670a5dca3c36b925937c1321c0f96a1abc8f
Andres G. Quina <andresgqjob@gmail.com>
2018-07-22 10:32:54 +0200

    MiBlog
    v1,0

---
+++ src/java/org/alexys/rest/modelo/Articulo.java
@@ -0,0 +1,12 @@
+
+package org.alexys.rest.modelo;
+
+import java.util.Calendar;
+
+public class Articulo {
+    private int id;
+    private String titulo;
+    private String contenido;
+    private Calendar creado;
+    private String autor;
+}
