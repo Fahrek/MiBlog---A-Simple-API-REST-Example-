2cc878f55a14970be3fcd205c3a651f376b28e59
Andres G. Quina <andresgqjob@gmail.com>
2018-07-22 15:06:48 +0200

    MiBlog03
    v1.2

--- src/java/org/alexys/rest/recursos/ArticuloRecurso.java
+++ src/java/org/alexys/rest/recursos/ArticuloRecurso.java
@@ -1,18 +1,38 @@
-
 package org.alexys.rest.recursos;
 
-import javax.ws.rs.GET;
+import java.util.List;
 import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import org.alexys.rest.modelo.Articulo;
+import org.alexys.rest.servicio.ArticuloServicio;
 
 @Path("/articulos")
 public class ArticuloRecurso {
+
+    ArticuloServicio servicio = new ArticuloServicio();
+
     @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public Articulo getArticulos(){
-        Articulo articulo = new Articulo(1, "Mi primer articulo", "Este es el contenido", "Andres");
-        return articulo;
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Articulo> getArticulos() {
+        return servicio.getArticulos();
+    }
+
+    @GET
+    @Path("/{articuloId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Articulo getArticulo(@PathParam("articuloId") int id) {
+        return servicio.getArticulo(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Articulo addArticulo(Articulo articulo) {
+        return servicio.addArticulo(articulo);
     }
 }
